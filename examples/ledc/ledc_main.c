29590d0ea4f9d1222924c7c9b2d07735899cad80
KyleLEI <leizhao2@gmail.com>
2018-02-24 16:22:41 +0800

    more arguments for ledc

--- examples/ledc/ledc_main.c
+++ examples/ledc/ledc_main.c
@@ -16,7 +16,7 @@
 //#include <stdlib.h>
 #include <stdio.h>
 #include <fcntl.h>
-#include <sched.h>
+#include <stdlib.h>
 #include <errno.h>
 #include <string.h>
 
@@ -37,11 +37,12 @@ int ledc_main(int argc, FAR char *argv[])
 #endif
 {
 	if(argc<2){
-		printf("Usage: ledc on/off\n");
+		printf("Usage: ledc [LED_ID] on/off\n");
 		return 1;
 	}
-	userled_set_t supported;
+
 	userled_set_t ledset;
+	userled_set_t supported;
 	printf("ledc: Opening %s\n", CONFIG_EXAMPLES_LEDC_DEVPATH);
 	int fd=open(CONFIG_EXAMPLES_LEDC_DEVPATH, O_WRONLY);
 	if (fd < 0){
@@ -50,22 +51,45 @@ int ledc_main(int argc, FAR char *argv[])
 	             strerror(errcode));
 	      return 1;
 	}
-	int ret=ioctl(fd, ULEDIOC_SUPPORTED,
-            (unsigned long)((uintptr_t)&supported));
-	if (ret < 0){
-		int errcode = errno;
-    		printf("ledc: ERROR: ioctl(ULEDIOC_SUPPORTED) failed: %s\n",
-           strerror(errcode));
-    	return 1;
-	}
-	printf("ledc: Supported LEDs 0x%02x\n", (unsigned int)supported);
 
-	if (!strcmp(argv[1],"on")){
-		printf("Turning on LED\n");
-		ledset=supported&0x00;
-	}else {
-		printf("Turning off LED\n");
-		ledset=supported&0xFF;
+	int ret;
+	ret = ioctl(fd, ULEDIOC_SUPPORTED,
+			(unsigned long) ((uintptr_t) &supported));
+	if (ret < 0) {
+		int errcode = errno;
+		printf("ledc: ERROR: ioctl(ULEDIOC_SUPPORTED) failed: %s\n",
+				strerror(errcode));
+		return 1;
+	}
+	printf("ledc: Supported LEDs 0x%02x\n", (unsigned int) supported);
+
+	if (argc == 2) {
+		if (!strcmp(argv[1], "on")) {
+			printf("Turning on LED\n");
+			ledset = supported & 0x00;
+		} else {
+			printf("Turning off LED\n");
+			ledset = supported & 0xFF;
+		}
+	} else {
+		userled_set_t ledstatus;
+		ret = ioctl(fd, ULEDIOC_GETALL,
+				(unsigned long) ((uintptr_t) &ledstatus));
+		if (ret < 0) {
+			int errcode = errno;
+			printf("ledc: ERROR: ioctl(ULEDIOC_GETALL) failed: %s\n",
+					strerror(errcode));
+			return 1;
+		}
+		printf("ledc: LEDs status 0x%02x\n", (unsigned int)ledstatus);
+		int setled = atoi(argv[1]);
+		if (!strcmp(argv[2], "on")) {
+			printf("Turning on LED%d\n", setled);
+			ledset = ledstatus & ~setled;
+		} else {
+			printf("Turning off LED%d\n", setled);
+			ledset = ledstatus | setled;
+		}
 	}
 
 	ret = ioctl(fd, ULEDIOC_SETALL, ledset);//setting
